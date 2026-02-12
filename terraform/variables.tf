56fe65355ed4f567a763ff76036419bdac802368
Paul Okoma <okomapaul@yahoo.com>
2026-02-12 00:13:38 -0500

    add terraform config for deploying to ec2

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable vpc_cidr_block {
+  default = "10.0.0.0/16"
+}
+variable subnet_cidr_block {
+  default = "10.0.10.0/24"
+}
+variable avail_zone {
+  default = "ca-central-1a"
+}
+variable env_prefix {
+  default = "dev"
+}
+variable my_ip {
+  default = "85.246.32.98/32"
+}
+variable jenkins_ip {
+  default = "143.110.218.223/32"
+}
+variable instance_type {
+  default = "t2.micro"
+}
+variable region {
+  default = "ca-central-1"
+}
